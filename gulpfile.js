d790b62b067131dc288146e9fc1b1c361b785aad
Andrea <takamiya.andrea@gmail.com>
2016-12-17 19:02:34 -1000

    Got titles and some images to print on page

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+
+
 const gulp = require('gulp');
 const scss = require('gulp-sass');
 const browserSync = require('browser-sync');
@@ -5,23 +7,23 @@ const browserSync = require('browser-sync');
 gulp.task('scss', () => {
   return gulp.src('./scss/*.scss')
     .pipe(scss())
-    .pipe(gulp.dest('./public/css'))
-})
+    .pipe(gulp.dest('./public/css'));
+});
 
 //watch makes it look at this directory and any directories within this directory. If there are any changes, the watch task triggers and runs the task called 'scss', which updates the app.css file
 gulp.task('watch', ['browserSync'], ()=> {
-  gulp.watch('./scss/**/*.scss', ['scss'])
-})
+  gulp.watch('./scss/**/*.scss', ['scss']);
+});
 
 gulp.task('browserSync', () => {
   return browserSync.init(['./scss', './public'], {   //if anything changes in the browser, then run the scss file.
     server: {
       baseDir: './public'
     }
-  })
-})
+  });
+});
 
-gulp.task('default', ['watch', 'scss'])   //run the default task, then run the watch task. If anything canges, then run the scss task.
+gulp.task('default', ['watch', 'scss']);   //run the default task, then run the watch task. If anything canges, then run the scss task.
 
 
 //the app.scss file is a style compiler, that runs to produce a css file.
--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,34 @@
+(function(window){
+
 console.log('Test app.js');
+
+function reqListener(){
+
+  var aww = JSON.parse(this.responseText);
+
+  for (var i = 0; i < 10; i++){
+
+    var titleDivCreate = document.createElement('div');
+    var awwTitle = document.createTextNode(aww.data.children[i].data.title);
+    titleDivCreate.appendChild(awwTitle);
+    document.body.appendChild(titleDivCreate);
+
+    var awwPic = document.createElement('img');
+    awwPic.setAttribute('src', aww.data.children[i].data.url);
+    awwPic.setAttribute("height", "200");
+    document.getElementById('img-1').appendChild(awwPic);
+  }
+
+  // console.log(aww.data.children[0].data.title);
+  // console.log(aww.data.children[1].data.title);
+
+}
+
+var oReq = new XMLHttpRequest();
+oReq.addEventListener('load', reqListener);
+oReq.open('GET', 'https://www.reddit.com/r/aww.json');
+oReq.send();
+
+
+}(window));
+
