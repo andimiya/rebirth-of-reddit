78de123e845906a67fc318b0f90d23e7cd234861
Andrea <takamiya.andrea@gmail.com>
2016-12-17 16:29:36 -1000

    Folder and docs setup

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp');
+const scss = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+gulp.task('scss', () => {
+  return gulp.src('./scss/*.scss')
+    .pipe(scss())
+    .pipe(gulp.dest('./public/styles'))
+})
+
+gulp.task('browserSync', () => {
+  return browserSync.init(['./scss', './public'], {   //if anything changes in the browser, then run the scss file.
+    server: {
+      baseDir: './public'
+    }
+  })
+})
+
+//watch makes it look at this directory and any directories within this directory. If there are any changes, the watch task triggers and runs the task called 'scss', which updates the app.css file
+gulp.task('watch', ['browserSync'], ()=> {
+  gulp.watch('./scss/**/*.scss', ['scss'])
+})
+
+
+gulp.task('default', ['watch', 'scss'])   //run the default task, then run the watch task. If anything canges, then run the scss task.
+
+
+//the app.scss file is a style compiler, that runs to produce a css file.
+//why is app.scss needed? can it handle logic?
+
