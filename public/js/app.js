d003b71599b875c1a871ae3ae85a27e04948c982
Andrea <takamiya.andrea@gmail.com>
2016-12-22 09:54:55 -1000

    Added an if to show only jpgs, no gifs

--- public/js/app.js
+++ public/js/app.js
@@ -9,47 +9,53 @@ function reqListener(){
     articlesHolder.className = 'articles-holder';
     document.getElementById('content-container').appendChild(articlesHolder);
 
-  for (var i = 1; i < 10; i++){
+  for (var i = 1; i < 30; i++){
 
-    var articleContainer = document.createElement('div');
-    articleContainer.className = 'article-container';
-    articlesHolder.appendChild(articleContainer);
-
-    //Image
-    var image = document.createElement('div');
-    image.className = 'item-image';
+    //Only Show Articles with JPGs, not GIFS
     var url = aww.data.children[i].data.preview.images[0].source.url;
-    articleContainer.appendChild(image);
-    image.style.backgroundImage = "url(" + url + ")";
+    if (url.match(/\.(jpg)/g)) {
 
-    //Article Title
-    var articleTitle = document.createElement('div');
-    articleTitle.className = 'headline';
-    articleTitle.innerHTML = aww.data.children[i].data.title;
-    articleContainer.appendChild(articleTitle);
+      var articleContainer = document.createElement('div');
+      articleContainer.className = 'article-container';
+      articlesHolder.appendChild(articleContainer);
 
-    //Author
-    var author = document.createElement('div');
-    author.className = 'author';
-    author.innerHTML = `by ${aww.data.children[i].data.author}`;
-    articleContainer.appendChild(author);
+      //Image
+      var image = document.createElement('div');
+      image.className = 'item-image';
+      url = aww.data.children[i].data.preview.images[0].source.url;
+      articleContainer.appendChild(image);
+      image.style.backgroundImage = "url(" + url + ")";
 
-    //Views - Author is the Parent
-    var views = document.createElement('span');
-    views.className = 'views';
-    views.innerHTML = `${aww.data.children[i].data.num_comments} comments`;
-    author.appendChild(views);
+      //Article Title
+      var articleTitle = document.createElement('div');
+      articleTitle.className = 'headline';
+      articleTitle.innerHTML = aww.data.children[i].data.title;
+      articleContainer.appendChild(articleTitle);
 
-    //Date - Author is the Parent
-    var dateDivCreate = document.createElement('span');
-    dateDivCreate.className = 'time-ago';
-    var date = moment.unix(aww.data.children[i].data.created_utc);
-    var today = new Date();
-    var duration = document.createTextNode(date.from(today));
-    dateDivCreate.innerHTML = duration;
-    author.appendChild(duration);
+      //Author
+      var author = document.createElement('div');
+      author.className = 'author';
+      author.innerHTML = `by ${aww.data.children[i].data.author}`;
+      articleContainer.appendChild(author);
+
+      //Views - Author is the Parent
+      var views = document.createElement('span');
+      views.className = 'views';
+      views.innerHTML = `${aww.data.children[i].data.num_comments} comments`;
+      author.appendChild(views);
+
+      //Date - Author is the Parent
+      var dateDivCreate = document.createElement('span');
+      dateDivCreate.className = 'time-ago';
+      var date = moment.unix(aww.data.children[i].data.created_utc);
+      var today = new Date();
+      var duration = document.createTextNode(date.from(today));
+      dateDivCreate.innerHTML = duration;
+      author.appendChild(duration);
+    }
   }
 }
+
 
 var oReq = new XMLHttpRequest();
 oReq.addEventListener('load', reqListener);
